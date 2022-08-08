e40c0c8c1dda0846bd22a03f6b01fd5c8345903d
Andrea Kalife <andreaklfdlg@gmail.com>
2022-08-08 15:57:26 -0500

    PDF File, Component Updates

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import resume from '../../assets/img/resume/resume.pdf';
+import resume from '../../assets/img/resume.pdf';
 import { ScaleFade } from '@chakra-ui/react';
 
 function Resume({ name }) {
